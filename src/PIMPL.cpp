41b1ab636e76a7b8d369354c88b2b85c9c9acfcb
r-tarnowski <radek.github@wp.pl>
2023-06-12 20:58:18 +0200

    Initial commit

---
+++ src/PIMPL.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+
+using std::cout;
+using std::endl;
+
+void printHeader() {
+   cout << endl;
+   cout << "================================================" << endl;
+   cout << "Design Patterns in Modern C++ by Dmitri Nesteruk" << endl;
+   cout << "                 PIMPL Idiom                    " << endl;
+   cout << "================================================" << endl;
+   cout << endl;
+}
+
+
+
+int main(int argc, char *argv[]) {
+   printHeader();
+
+   return 0;
+}
